2632ec30fbdedf3cd7ac090f5871a236a119f9be
ShareefRahat <imr4h47@gmail.com>
2022-07-07 12:12:09 +0600

    schema and union type is understood

--- output/script.js
+++ output/script.js
@@ -1,2 +1,17 @@
 "use strict";
+const country = "Bangladesh";
 console.log("Hello");
+let person = "Rahim";
+console.log(person);
+let age = 45;
+let student;
+student = "Karim";
+//UNION Type
+let a;
+a = "something";
+a = 4;
+// a = {} we can no set the value as object as it is string or number type
+//Array or Object Schema or data structure.
+let arry = [];
+arry.push("something", 56);
+// arry.push({}) : here we can not push object in this array because it is string or number type array. this is same for an object schema.
